040eab261b06eca2b04db1c89870948d7f6b71b3
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-10-03 20:52:08 +0300

    Add new dialog
    dobavih nov dialog

---
+++ dialog nov.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std
+int main()
+{
+    cout<<"Kak se kazvash";
+    char ime [15];
+    cin>>ime;
+    cout<<"Zdravei"<<ime<<"!"<<endl;
+    cout<<Na kolko si godini?;
+    char godini [2];
+    cin>>godini;
+    cout<<"Koga si roden"<<data<<"?"<<endl;
+    cout<<Koq ti e lubimata hrana?;
+    char hrana [10];
+    cin>>hrana
+    cout<<A lubimata napitka<<napitka<<"?"<<endl;
+    cout<<Kakvo ti e hobito?;
+    char hobi [20];
+    cin>>hobi;
+    cout<<A ot kolko vreme se zanimavash s tova<<vreme<<"?"<<endl;
+    cout<<Kato kakav iskash da rabotish, kogato zavarshish?;
+    char kariera [20];
+    cin>>kariera
+    cout<<A siguren li si v namereniqta si<<namereniq<<"?"<<endl;
+    cout<<Beshe mi priqtno da se zapoznaem.Nadqvam se da se vidim pak;
+    char sboguvane [30]
+    cout<<Bye<<razdqla<<"!"<<endl;
+    return 0;
+}
