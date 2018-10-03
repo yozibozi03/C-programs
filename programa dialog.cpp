a7767edcb7ebedbec0c52f856dfa4ad412b7271e
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-10-03 11:32:31 +0300

    add files
    dobavih dialog

---
+++ programa dialog.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Kak se kazvash?";
+    char ime[15];
+    cin>>ime;
+    cout<<"Zdravei"<<ime<<"!"<<endl;
+    cout<<"Na kolko godini si?";
+
+    char godini [2]
+    return 0;
+}
